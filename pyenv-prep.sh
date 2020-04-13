94181994ebd894c784dec22ab32634842d3bf7f7
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2020-04-13 19:40:08 +0600

    virtualenv setup script

--- pyenv-prep.sh
+++ pyenv-prep.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
-# python virtual environment creation script for python project
+# virtual environment setup script for python projects
 
+PROJECT=$1
+
+cd /home/workspace/"$PROJECT"
+if [ -d py3env ]; then
+    source py3env/bin/activate
+    cd "$PROJECT"
+else
+    virtualenv -p /usr/bin/python3 py3env
+    source py3env/bin/activate
+    cd "$PROJECT"
+fi
+
+# running isort
+docker-compose run app bash -c "isort"
+
+# running pylint
+docker-compose run app bash -c "pylint"
+
+# running test
+docker-compose run app bash -c "python manage.py test"
+
+deactivate
